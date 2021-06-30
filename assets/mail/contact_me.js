e3874b53a899aef65f8eaf23598613b830ce68b4
Julietkays <84806428+Julietkays@users.noreply.github.com>
2021-06-30 23:45:13 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://9n2hdn7220.execute-api.eu-west-1.amazonaws.com",
+                url: "https://ohy301nfci.execute-api.eu-west-1.amazonaws.com/test/apilambdadb",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
